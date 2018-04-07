5cf49a864cfd9c28b77053dd691a52f48b9ac8c8
sharpwaveripple <jonathantay193@gmail.com>
2018-04-07 21:13:43 +0100

    added basic R package installation

---
+++ R/install_R_pkgs.R
@@ -0,0 +1,13 @@
+pkgs <- c("tidyverse",
+          "psych",
+          "mice",
+          "lme4",
+          "nlme",
+          "afex",
+          "phia",
+          "snow",
+          "paran",
+          "pROC")
+
+install.packages(pkgs, dependencies = T)
+
