8dba2a9eab52f29a047f34e2afd050080c16b746
Jonathan Tay <jonathantay193@gmail.com>
2018-11-05 15:48:12 +0000

    changed alpha to use precision consistent with field

--- R/format_output.R
+++ R/format_output.R
@@ -19,12 +19,21 @@ format_corr <- function(corr_test_obj, digits = 2, p_digits = 3, p_under = .001)
   return(corr_table)
 }
 
-format_alpha <- function(alpha_obj, digits = 3) {
-  fmt_str <- paste("%.", digits, "f", sep="")
-  response_freq <- (alpha_obj$response.freq) * 100
-  item_stats <- cbind(response_freq, alpha_obj$alpha.drop['std.alpha'])
-  item_stats[] <- sprintf(fmt_str, as.matrix(item_stats))
-  return(item_stats)
+format_alpha <- function(alpha_obj) {
+  # Summarizes input from psych::alpha()
+  stats <- list()
+  scale_stats <- alpha_obj$total[c("std.alpha", "mean", "sd")]
+  scale_stats[1] <- sprintf("%.2f", scale_stats[1])
+  scale_stats[2:3] <- sprintf("%.1f", scale_stats[2:3])
+  stats[['alpha']] <- scale_stats[1]
+  stats[['mean']] <- paste(scale_stats[2], " (", scale_stats[3], ")", sep="")
+  response_freq <- alpha_obj$response.freq * 100
+  response_freq[] <- sprintf("%.1f", as.matrix(response_freq))
+  alpha_drop <- alpha_obj$alpha.drop['std.alpha']
+  alpha_drop[] <- sprintf("%.2f", as.matrix(alpha_drop))
+  item_stats <- cbind(response_freq, alpha_drop)
+  stats[['items']] <- item_stats
+  return(stats)
 }
 
 extract_fit <- function(lavaan_fit, digits = 2, p_digits = 3, scaled = TRUE) {
