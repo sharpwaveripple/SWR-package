0ee1da5c3f1aee22faf4cf882e257252f2d8ea7f
Jonathan Tay <jonathantay193@gmail.com>
2018-11-05 15:23:16 +0000

    added digits arguments specifically for p

--- R/format_output.R
+++ R/format_output.R
@@ -7,19 +7,27 @@ format_p <- function(p, digits = 3, p_under = .001) {
   return(p)
 }
 
-format_corr <- function(corr_test_obj, digits = 3, p_under = .001) {
+format_corr <- function(corr_test_obj, digits = 2, p_digits = 3, p_under = .001) {
   # Takes the output of psych::corr.test() and formats it as a r (p) matrix
   fmt_str <- paste("%.", digits, "f", sep="")
   r <- corr_test_obj$r
   r[] <- sprintf(fmt_str, r)
-  p <- format_p(corr_test_obj$p, digits, p_under)
+  p <- format_p(corr_test_obj$p, p_digits, p_under)
   corr_table <- r
   corr_table[] <- paste(r, " (", p, ")", sep="")
   diag(corr_table) <- ""
   return(corr_table)
 }
 
-extract_fit <- function(lavaan_fit, digits = 3, scaled = TRUE) {
+format_alpha <- function(alpha_obj, digits = 3) {
+  fmt_str <- paste("%.", digits, "f", sep="")
+  response_freq <- (alpha_obj$response.freq) * 100
+  item_stats <- cbind(response_freq, alpha_obj$alpha.drop['std.alpha'])
+  item_stats[] <- sprintf(fmt_str, as.matrix(item_stats))
+  return(item_stats)
+}
+
+extract_fit <- function(lavaan_fit, digits = 2, p_digits = 3, scaled = TRUE) {
   fmt_str <- paste("%.", digits, "f", sep="")
   fit_ind <- c("chisq", "pvalue", "rmsea", "rmsea.ci.lower", "rmsea.ci.upper",
                "cfi", "tli", "srmr")
@@ -38,3 +46,4 @@ extract_fit <- function(lavaan_fit, digits = 3, scaled = TRUE) {
   fit_list <- list(chisq=chisq, rmsea=rmsea, cfi=cfi, tli=tli, srmr=srmr)
   return(fit_list)
 }
+
