4a8fd3b5b1c269f9ff352159cda9cd7d6809cd13
Jonathan Tay <jonathantay193@gmail.com>
2021-03-14 20:46:20 +0000

    adde max and min n

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,18 @@
+#' Find n max values in a vector
+#'
+#' \code{n_max} takes a vector and finds the n largest numbers
+#'
+#' @param x Numerical vector
+#' @param n Number of largest elements to return
+#' @export
+n_max <- function(x, n=1) {
+  tail(sort(x), n)
+}
+
+n_min <- function(x, n=1) {
+  head(sort(x), n)
+}
+
 count_long_resp <- function(df, id_col, var) {
   id_list <- unique(df[[id_col]])
   for (id in id_list) {
